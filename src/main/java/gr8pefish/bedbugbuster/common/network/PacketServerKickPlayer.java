63aba886917a81cc00a474cc1bb6b76a8fef2a48
Max Wason <xwason@gmail.com>
2016-12-02 10:28:27 -0700

    Add Logger (and use it)

--- src/main/java/gr8pefish/bedbugbuster/common/network/PacketServerKickPlayer.java
+++ src/main/java/gr8pefish/bedbugbuster/common/network/PacketServerKickPlayer.java
@@ -1,5 +1,6 @@
 package gr8pefish.bedbugbuster.common.network;
 
+import gr8pefish.bedbugbuster.common.lib.Logger;
 import io.netty.buffer.ByteBuf;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
@@ -27,7 +28,7 @@ public class PacketServerKickPlayer implements IMessage {
             try {
                 ctx.getServerHandler().playerEntity.connection.kickPlayerFromServer("Leaving bed (and server)");
             } catch (Exception e) {
-                System.out.println("Can't kick player!"); //ToDo: Logger
+                Logger.error("Can't kick player!");
             }
             System.out.println("wtf");
             return null; //no return message
