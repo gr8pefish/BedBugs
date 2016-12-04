f12e9ade458845d790201920fdd40cbb5506bc3b
Max Wason <xwason@gmail.com>
2016-12-03 17:12:37 -0700

    Add tootlitp delay and kick button appearance delay

--- src/main/java/gr8pefish/bedbugs/client/event/ClientEventHandler.java
+++ src/main/java/gr8pefish/bedbugs/client/event/ClientEventHandler.java
@@ -19,6 +19,15 @@ public class ClientEventHandler {
 
     private KickButton kickButton;
 
+    private boolean showButton;
+    private long prevSystemVisibleTime;
+    private long visibleTime;
+    private final int visibleShow = 500; //0.5 seconds
+
+    private long prevSystemHoverTime;
+    private long hoverTime;
+    private final int hoverShow = 1000; //1 second
+
     @SubscribeEvent
     public void onSleepGui(GuiScreenEvent.InitGuiEvent.Post event){
         if (event.getGui() instanceof GuiSleepMP) { //sleep GUI
@@ -29,33 +38,90 @@ public class ClientEventHandler {
             kickButton.visible = false; //set invisible
             event.getButtonList().add(kickButton); //add my own Kick Button, default invisible
 
+            showButton = false;
+            prevSystemVisibleTime = 0;
+            visibleTime = 0;
+            prevSystemHoverTime = 0;
+            hoverTime = 0;
+
             event.setButtonList(list); //update button list
         }
     }
 
+    /**
+     * Makes the button visible after 1 second.
+     * @param event
+     */
     @SubscribeEvent
     public void onClick(GuiScreenEvent.ActionPerformedEvent.Pre event) { //ToDo: try with post
         if (event.getGui() instanceof GuiSleepMP) { //sleep GUI
+
             if (event.getButton().id == 1) { //normal 'Leave Bed' button clicked
                 for (GuiButton button : event.getButtonList()) { //loop through all buttons on screen
                     if (button instanceof KickButton) { //find kick button
-                        button.visible = true; //set it to visible
+                        if (showButton == false) showButton = true;
+                        break; //stop looping
                     }
                 }
                 event.setCanceled(true); //ToDo: testing purposes, remove in production!
             }
+
             if (event.getButton() instanceof KickButton) {
                 PacketHandler.HANDLER.sendToServer(new PacketServerKickPlayer());
             }
         }
     }
 
+    /**
+     * Draws the tooltip after a delay.
+     * @param event - drawScreenEvent
+     */
     @SubscribeEvent
     public void onHover(GuiScreenEvent.DrawScreenEvent.Post event) {
         if (event.getGui() instanceof GuiSleepMP) {
+
+            //show tooltip after 1 second delay
+            KickButton curr = null;
             if (kickButton != null && kickButton.visible && kickButton.isMouseInButton(event.getMouseX(), event.getMouseY())) {
-                GuiUtils.drawHoveringText(kickButton.getTooltipLines(), event.getMouseX(), event.getMouseY(), event.getGui().width, event.getGui().height, 300, Minecraft.getMinecraft().fontRendererObj);
+                curr = kickButton;
             }
+            if (curr != null){
+                long systemTime = System.currentTimeMillis();
+                if(prevSystemHoverTime != 0) {
+                    hoverTime += systemTime - prevSystemHoverTime;
+                }
+                prevSystemHoverTime = systemTime;
+                if(hoverTime > hoverShow) {
+                    if (Minecraft.getMinecraft().currentScreen instanceof GuiSleepMP)
+                        GuiUtils.drawHoveringText(kickButton.getTooltipLines(), event.getMouseX(), event.getMouseY(), event.getGui().width, event.getGui().height, 300, Minecraft.getMinecraft().fontRendererObj);
+                }
+            }else{
+                hoverTime = 0;
+                prevSystemHoverTime = 0;
+            }
+
+            //Deal with showing the button after 0.5 second delay
+            if (showButton){
+                long systemTime = System.currentTimeMillis();
+                if(prevSystemVisibleTime != 0) {
+                    visibleTime += systemTime - prevSystemVisibleTime;
+                }
+                prevSystemVisibleTime = systemTime;
+                if(visibleTime > visibleShow) {
+                    if (Minecraft.getMinecraft().currentScreen instanceof GuiSleepMP) {
+                        kickButton.visible = true;
+                    }
+                }
+                if (!(Minecraft.getMinecraft().currentScreen instanceof GuiSleepMP)) {
+                    showButton = false;
+                    visibleTime = 0;
+                    prevSystemVisibleTime = 0;
+                }
+            } else {
+                visibleTime = 0;
+                prevSystemVisibleTime = 0;
+            }
+
         }
     }
 
